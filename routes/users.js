1168a1c524ebeb8870a8fa3c254338e34bed802c
chanstag <chandler.staggs945@topper.wku.edu>
2018-03-17 18:45:00 -0500

    recreated using express generator and added upload

---
+++ routes/users.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var formidable = require('formidable');
+var http = require('http');
+var util = require('util');
+var fs = require('fs');
+var stringify = require('json-stringify-safe');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  if((req.session && req.session.username))
+		res.render('users', {user: req.session.username});
+	else{
+		res.redirect('/login');
+	}
+  // res.send('respond with a resource');
+});
+
+router.post('/upload', function(req, res, next){
+  console.log('entry');
+  if((req.session && req.session.username)){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+       if(err){
+         console.log(err);
+         res.send('Error uploading file')
+       }
+       else{
+         var x = JSON.parse(stringify(files));
+        console.log(x.file.path);
+        fs.createReadStream(x.file.path).pipe(fs.createWriteStream('./uploads/'+x.file.name));
+        res.send(util.inspect({fields: fields, files: files}));
+       }
+    });
+
+  }
+  else{
+    message = "you cannot upload, you're not logged in";
+    res.redirect('/login')
+    
+  }
+});
+
+module.exports = router;

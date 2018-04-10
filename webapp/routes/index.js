030c61be832fb872530bd91075628f85b3a466de
Jamie Thomas <31860384+JamieThomas-WKU@users.noreply.github.com>
2018-04-10 09:36:20 -0500

    Add files via upload

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var tools = require('../tools');
+
 // Remove after done testing
-const testJSON = require('testJSON');
+const testJSON = require('../testJSON');
 
 router.get('/', function(req, res){
 
@@ -10,26 +12,16 @@ router.get('/', function(req, res){
     title: 'Home',
     jobs: testJSON.jobs,
     files: testJSON.files,
+    tools: tools,
   });
 });
 
-router.get('/createjob/scripts', function(req, res){
-	var scriptList = [];
-	for(var x = 0; x < testJSON.scripts.length; x++) {
-		scriptList.push(testJSON.scripts[x].name);
-	}
-  res.render('createjob/scripts', {
-    title: 'Create Job',
-    scriptsList: scriptList,
-  });
-});
-
-router.get('/createjob/commands', function(req, res){
-  res.render('createjob/commands', {
+router.get('/createjob', function(req, res){
+  res.render('createjob', {
     title: 'Create Job',
     script: testJSON.scripts[0],
+    tools: tools,
   });
 });
 
 module.exports = router;
-

9bf291b67d6a705d9fcde7e24ab69ec8b7f5de9d
Chandler V. Staggs <chandler.staggs945@topper.wku.edu>
2018-04-11 10:48:36 -0500

    Update createjob.js

--- webapp/routes/createjob.js
+++ webapp/routes/createjob.js
@@ -80,45 +80,43 @@ router.post('/sendjob', function(req, res){
       
       // call job to the backend
       
-      /*
-
-       //need to make sure this name matches existing script 
-       var script = req.body.script;
-       var arr = req.body.args
-       var demo_directory = /data/demoDir;
-       var bash = "#!/bin/bash\n\n"+script+" "+arr[0];
-       //save this to file system
-       fs.writeFile(demo_directory+"/test.sh", bash, (err) => { 
-           if (err) throw err;
-               // success case, the file was saved
-               console.log('bash file saved!');
-       });
-       //create yaml file here and then pass that as an arg to the exec command
-       yaml.load('template.yml', function(result)
-       {   
-           console.log(yaml.stringify(result, 4));
-           nativeObject = result;
-           nativeObject.metadata.name = script;
-           nativeObject.metadata.spec.spec.containers.name = [script];
-           nativeObject.metadata.spec.spec.containers.image = "chanstag/"+script;
-           nativeObject.metadata.spec.spec.containers.command = arr;
-           nativeObject.metadata.spec.spec.containers.restartPolicy = "Never"
-           nativeObject.metadata.spec.spec.containers.volumeMounts = demo_directory;
-           fs.writeFile("./template.yaml", nativeObject, (err) => { 
-               if (err) throw err;
-               
-                   // success case, the file was saved
-                   console.log('yaml file saved!');
-   
-           }).exec("kubectl create -f ./template.yaml", (err, stdout, stderr) => {
-               if (err) {
-                 console.error(err);
-                 return;
-               }
-               console.log(stdout);
-             });
-      })
-      */
+     
+        //need to make sure this name matches existing script 
+        var script = JSON.parse(req.body).script;
+        var arr = JSON.parse(req.body).file;
+        var demo_directory = /data/demoDir;
+        var bash = "#!/bin/bash\n\n"+script+" "+arr;
+        //save this to file system
+        fs.writeFile(demo_directory+"/test.sh", bash, (err) => { 
+            if (err) throw err;
+                // success case, the file was saved
+                console.log('bash file saved!');
+        });
+        //create yaml file here and then pass that as an arg to the exec command
+        yaml.load('template.yml', function(result)
+        {   
+            console.log(yaml.stringify(result, 4));
+            nativeObject = result;
+            nativeObject.metadata.name = script;
+            nativeObject.metadata.spec.spec.containers.name = [script];
+            nativeObject.metadata.spec.spec.containers.image = "chanstag/"+script;
+            nativeObject.metadata.spec.spec.containers.command = arr;
+            nativeObject.metadata.spec.spec.containers.restartPolicy = "Never"
+            nativeObject.metadata.spec.spec.containers.volumeMounts = demo_directory;
+            fs.writeFile("./template.yaml", nativeObject, (err) => { 
+                if (err) throw err;
+                
+                    // success case, the file was saved
+                    console.log('yaml file saved!');
+    
+            }).exec("kubectl create -f ./template.yaml", (err, stdout, stderr) => {
+                if (err) {
+                  console.error(err);
+                  return;
+                }
+                console.log(stdout);
+              });
+        })
 });
 
 module.exports = router;

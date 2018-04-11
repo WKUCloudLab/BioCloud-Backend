04451d082d121e994a6393b5cc87fa257990ab98
chanstag <chanstag@live.com>
2018-04-11 10:51:33 -0500

    have to push this

--- webapp/routes/createjob.js
+++ webapp/routes/createjob.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const yaml = require('yamljs');
+const fs = require('fs');
+const { exec } = require('child_process');
 
 // Remove after done testing
 const testJSON = require('../testJSON');
@@ -44,7 +47,44 @@ router.get('/', function(req, res){
 router.post('/sendjob', function(req, res){
   console.log('recieving job');
 
-  console.log(req.data);
+	//console.log(req.data);
+       //need to make sure this name matches existing script 
+       var script = "fastqc";
+       var arr = "SAMPLE"
+       var demo_directory = "/data/demoDir";
+       var bash = "#!/bin/bash\n\n"+script+" "+arr;
+       console.log("creating bash string");
+	//save this to file system
+       fs.writeFile(demo_directory+"/test.sh", bash, (err) => { 
+           if (err) throw err;
+               // success case, the file was saved
+               console.log('bash file saved!');
+       });
+       //create yaml file here and then pass that as an arg to the exec command
+       yaml.load('template.yml', function(result)
+       {   
+           console.log(yaml.stringify(result, 4));
+           nativeObject = result;
+           nativeObject.metadata.name = script;
+           nativeObject.metadata.spec.spec.containers.name = [script];
+           nativeObject.metadata.spec.spec.containers.image = "chanstag/"+script;
+           nativeObject.metadata.spec.spec.containers.command = arr;
+           nativeObject.metadata.spec.spec.containers.restartPolicy = "Never"
+           nativeObject.metadata.spec.spec.containers.volumeMounts = demo_directory;
+           fs.writeFile("./template.yaml", nativeObject, (err) => { 
+               if (err) throw err;
+               
+                   // success case, the file was saved
+                   console.log('yaml file saved!');
+   
+           }).exec("kubectl create -f ./template.yaml", (err, stdout, stderr) => {
+               if (err) {
+                 console.error(err);
+                 return;
+               }
+               console.log(stdout);
+             });
+	});
 
       // create new job entry in database
 			var DB_USER = process.env.DB_USER;
@@ -66,8 +106,8 @@ router.post('/sendjob', function(req, res){
 			});
 
 			var date = new Date().toISOString().slice(0, 19).replace('T', ' ');
-			var sql = "INSERT INTO files (name, status, start, end, next_job, script_id, user_id, pipeline_id, commands)" +
-				" VALUES ('Test Job', 'INPROCESS', '"+date+"', '"+null+"', '"+null+"', 'FastQC', '0000', 'null', 'null')";
+			var sql = "INSERT INTO jobs (name, status, start, end, next_job, script_id, user_id, pipeline_id, commands)" +
+				" VALUES ('Test Job', 'INPROCESS', '"+date+"', '"+date+"', '1', 'FastQC', '0000', '1', '1')";
 			console.log(sql);
 			connection.query(sql, function (err, result) {
 				if (err) throw err;
@@ -75,15 +115,15 @@ router.post('/sendjob', function(req, res){
 			});
 
       connection.end();
-      
+});      
       // call job to the backend
       
-      /*
-
+      
+/*	
        //need to make sure this name matches existing script 
        var script = req.body.script;
        var arr = req.body.args
-       var demo_directory = /data/demoDir;
+       var demo_directory = "/data/demoDir";
        var bash = "#!/bin/bash\n\n"+script+" "+arr[0];
        //save this to file system
        fs.writeFile(demo_directory+"/test.sh", bash, (err) => { 
@@ -116,7 +156,7 @@ router.post('/sendjob', function(req, res){
                console.log(stdout);
              });
       })
-      */
-});
+  */    
+
 
 module.exports = router;

6f8f063a96b4b5dea0d0d416ecb1bbb885fd9296
chanstag <chandler.staggs945@topper.wku.edu>
2018-10-17 23:30:36 -0500

    Work In Progress: Converting to Sequelize

--- sqlize/models/pipeline.js
+++ sqlize/models/pipeline.js
@@ -1,7 +1,10 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Pipeline = sequelize.define('Pipeline', {
-    id: DataTypes.INTEGER
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    }
   }, {});
   Pipeline.associate = function(models) {
     // associations can be defined here
